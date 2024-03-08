2db00b030915184e3fbe90b3d088b1491d79bc73
Alex <alex.crickmay@gmail.com>
2024-03-08 19:22:49 +0000

    initial commit

---
+++ functions.php
@@ -0,0 +1,53 @@
+<?php
+	
+	//Disable the admin bar on the front end
+	show_admin_bar(false);
+
+	//Post thumbnail support
+	add_theme_support('post-thumbnails');
+
+	//Register Menus
+	register_nav_menus(array(
+		'main' => 'Main Menu'
+	));
+	
+	//Remove junk from wp_head
+	remove_action('wp_head', 'rsd_link'); // Removes the Really Simple Discovery link
+	remove_action('wp_head', 'wlwmanifest_link'); // Removes the Windows Live Writer link
+	remove_action('wp_head', 'wp_generator'); // Removes the WordPress version
+	remove_action('wp_head', 'feed_links', 2); // Removes the RSS feeds
+	remove_action('wp_head', 'feed_links_extra', 3); // Removes all other RSS links
+	remove_action('wp_head', 'index_rel_link'); // Removes the index page link
+	remove_action('wp_head', 'start_post_rel_link', 10, 0); // Removes the random post link
+	remove_action('wp_head', 'parent_post_rel_link', 10, 0); // Removes the parent post link
+	remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0); // Removes the next and previous post links
+	remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0); // Remove shortlink
+	remove_action('wp_head', 'print_emoji_detection_script', 7); // Remove emoji junk
+	remove_action('wp_print_styles', 'print_emoji_styles'); // Remove emoji junk
+	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' ); // Remove emoji junk
+	remove_action( 'admin_print_styles', 'print_emoji_styles' ); // Remove emoji junk
+
+	add_action( 'wp_enqueue_scripts', 'remove_block_css', 100 ); // Remove wp-block render junk
+		function remove_block_css() {
+		wp_dequeue_style( 'wp-block-library' ); // WordPress core
+		wp_dequeue_style( 'wp-block-library-theme' ); // WordPress core
+		wp_dequeue_style( 'wc-block-style' ); // WooCommerce
+		wp_dequeue_style( 'storefront-gutenberg-blocks' ); // Storefront theme
+		wp_dequeue_style( 'global-styles' ); // Inline styling
+	}
+
+	// Remove image size limit
+	add_filter( 'big_image_size_threshold', '__return_false' );
+	
+	// Custom excerpt size
+	function excerpt($limit) {
+    	$excerpt = explode(' ', get_the_excerpt(), $limit);
+		if (count($excerpt)>=$limit) {
+			array_pop($excerpt);
+			$excerpt = implode(" ",$excerpt).' ... <a href="'.get_permalink($post->ID).'" class="read-more">'.'read more &rarr;'.'</a>';
+		} else {
+			$excerpt = implode(" ",$excerpt);
+		} 
+		$excerpt = preg_replace('`\[[^\]]*\]`','',$excerpt);
+		return $excerpt;
+    }
---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <?php if (is_page(2)) { ?>
+	<title>Nexor</title>
+    <?php } elseif (is_single()) { ?>
+	<title>Nexor - <?php wp_title(''); ?></title>
+    <?php } ?>
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=5.0, user-scalable=yes">
+	<link rel="icon" href="<?php bloginfo('template_directory'); ?>/img/favicon.png" type="image/x-icon"/>
+	<?php wp_head(); ?>
+    <link href="//fonts.googleapis.com" rel="preconnect">
+    <link href="//fonts.gstatic.com" rel="preconnect" crossorigin>
+    <link href="//fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,600;1,300;1,600&display=swap" rel="stylesheet">
+	<link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet">
+	<link href="<?php bloginfo('template_directory'); ?>/assets/application.css" rel="stylesheet">
+</head>
+
+<body <?php body_class(); ?>>
