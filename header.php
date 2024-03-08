588d58e06cebf024972633001de8c413d2d14358
Alex <alex.crickmay@gmail.com>
2024-03-08 19:55:03 +0000

    basic site header

--- header.php
+++ header.php
@@ -12,9 +12,24 @@
 	<?php wp_head(); ?>
     <link href="//fonts.googleapis.com" rel="preconnect">
     <link href="//fonts.gstatic.com" rel="preconnect" crossorigin>
-    <link href="//fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,600;1,300;1,600&display=swap" rel="stylesheet">
+    <link href="//fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,500;1,300;1,600&display=swap" rel="stylesheet">
 	<link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet">
 	<link href="<?php bloginfo('template_directory'); ?>/assets/application.css" rel="stylesheet">
 </head>
 
 <body <?php body_class(); ?>>
+
+	<header>
+		<div class="wrap wrap--x-large">
+			<div class="inner">
+				<div class="branding">
+					<a href="<?php bloginfo('url'); ?>" aria-label="<?php bloginfo('name'); ?>">
+						<img src="<?php bloginfo('template_directory'); ?>/assets/logo.png" width="120" height="74" alt="<?php bloginfo('name'); ?>">
+					</a>
+				</div>
+				<nav>
+					<?php wp_nav_menu(array('theme_location'=>'main')); ?>
+				</nav>
+			</div>
+		</div>
+	</header>
